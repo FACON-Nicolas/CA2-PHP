d33e7618a5804b6ae3e66fe85f26dc6f1ff68785
FACON-Nicolas <faconicolas@gmail.com>
2023-02-27 10:56:27 +0000

    add style for contact form

--- contact.php
+++ contact.php
@@ -5,6 +5,10 @@
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
   </div>
 
+    <form action="">
+
+    </form>
+
 </main><!-- /.container -->
     <script src="js/bootstrap.bundle.min.js"></script>
   </body>
