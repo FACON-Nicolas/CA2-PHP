724f7de51f7d6076e2adc9813835bdc7b5853a17
FACON-Nicolas <faconicolas@gmail.com>
2023-03-06 10:56:09 +0000

    add content top index page

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php include "includes/header.php"?>
+<?php include "includes/header.php" ?>
+
+<div class="pt-5 pb-3">
+    <h1 class="card-title fw-bolder game-main-title">MY<br/>NEW<br/>GAME</h1>
+    <img class="main-img" src="https://i.redd.it/cd46ipwu48i51.jpg">
+    <img class="game-main-img" src="https://applebygames.co.nz/wp-content/uploads/2023/02/CALL-OF-DUTY-COLD-WAR-XBOX-ONE-GAME.png"
+</div>
+
 <main class="container">
   <div class="starter-template text-center">
     <h1>Bootstrap starter template</h1>
