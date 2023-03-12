d79c3c9124e0a46e048d2fff56b90bdffb40ce82
FACON-Nicolas <faconicolas@gmail.com>
2023-03-12 10:13:48 +0000

    add single game page

--- game.php
+++ game.php
@@ -1,37 +1,64 @@
-<?php include "includes/header.php" ?>
+<?php
 
-<!--
-`website` TEXT NOT NULL,
-`media_url` TEXT NOT NULL
+function get_dev(int $id, $db) {
+    $statement = $db->prepare('SELECT DEVELOPER.* 
+                            FROM DEVELOPER JOIN DEVELOP 
+                            ON DEVELOPER.name = DEVELOP.name
+                            WHERE id_game =' . $id);
+    $statement->execute();
+    $devs = $statement->fetchAll();
+    $statement->closeCursor();
+    return $devs;
+}
 
--->
+$query = [];
+
+try {
+    include "includes/header.php";
+    include('database.php');
+
+    $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $parts = parse_url($url);
+    parse_str($parts['query'], $query);
+
+    $id = $query['id'];
+
+    $statement = $db->prepare("SELECT * FROM `GAME` WHERE `id`=$id");
+    $statement->execute();
+    $games = $statement->fetchAll();
+    if (!$games) throw new Exception();
+} catch (Exception $e) {
+    header("Location: games.php");
+}
+
+?>
+
 <main class="container">
   <div class="starter-template">
-    <h1>Add a game</h1>
-      <form action="add_game.php" method="post">
-          <div class="form-group">
-              <label for="name">Name: </label>
-              <input type="text" class="form-control" name="name" placeholder="name" required>
+      <?php foreach($games as $game): ?>
+          <h1 class="text-center modal-title"><?php echo $game['name'] ?></h1>
+              <div class="justify-content-center game-individual">
+                  <img class="game-pic" src="<?php echo $game['media_url'] ?>">
+                  <p class="fw-bold fs-3"><?php echo $game['name'] ?></p>
+                  <p class="fw-bold fs-3">Publisher: <a class="a" href="<?php echo $game['website'] ?>" target="_blank"><?php echo $game['publisher'] ?></a></p>
+                  <p class="fw-bold fs-3">
+                  <?php $tab = get_dev($game['id'], $db);
+                  if (count($tab) > 1) {
+                      echo "Developers: ";
+                      echo "<ul>";
+                      foreach ($tab as $dev) {
+                          echo "<li>" . $dev['name'] . "</li>";
+                      }
+                      echo '</ul>';
+                  } else {
+                      echo "Developer: " . $tab[0]["name"] . "<br/>";
+                  }
+                  ?>
+                  </p>
+                  <p class="fw-bold fs-3"> Description </p>
+                  <p><?php echo $game['description']; ?></p>
           </div>
-          <div class="form-group">
-              <label for="year">Year: </label>
-              <input type="text" class="form-control" name="year" placeholder="year" required>
-          </div>
-          <div class="form-group">
-              <label for="email">Publisher: </label>
-              <input type="text" class="form-control" name="publisher" placeholder="publisher" required>
-          </div>
-          <div class="form-group">
-              <label for="email">Website Publisher: </label>
-              <input type="text" class="form-control" name="website" placeholder="website" required>
-          </div>
-          <div class="form-group">
-              <label for="email">url picture: </label>
-              <input type="text" class="form-control" name="media_url" placeholder="url picture" required>
-          </div>
-          <button class="btn btn-primary" type="submit">Button</button>
-      </form>
-    <p class="lead"></p>
+      <?php endforeach; ?>
   </div>
 
 </main><!-- /.container -->
